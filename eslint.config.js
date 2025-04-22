a2d2a3ad273a1f1a07a658b22b02e0f50800a72d
Ishida Amuna <134792769+ishidaamna@users.noreply.github.com>
2025-04-22 22:41:55 +0900

    Add ESLint and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import configPrettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  // Base ESLint recommended config
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser
+    },
+    plugins: {
+      react: pluginReact
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off"
+    }
+  },
+  {
+    ...configPrettier
+  }
+]);
