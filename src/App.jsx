20f416dfb0f4723eaf9a48c3107d78b5d2d7f2d6
Ishida Amuna <134792769+ishidaamna@users.noreply.github.com>
2025-04-22 22:43:46 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import UserInput from "./components/UserInput";
-import Results from "./components/Results";
+import { useState } from 'react';
+import Header from './components/Header';
+import UserInput from './components/UserInput';
+import Results from './components/Results';
 
 function App() {
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
-    duration: 10,
+    duration: 10
   });
 
   const inputIsValid = userInput.duration >= 1;
@@ -16,7 +16,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: +newValue,
+        [inputIdentifier]: +newValue
       };
     });
   }
@@ -25,9 +25,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      {!inputIsValid && (
-      <p className="center">Please enter a duration greater than zero.</p>)}
-     {inputIsValid && <Results input={userInput} />}
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
